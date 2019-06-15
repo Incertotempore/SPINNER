8f07b9c2cc846618c261ee830fbb3119e1f9f83c
ivano panizza <ivano.panizza@libero.it>
2019-06-15 13:59:44 +0200

    content placeholder added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/tutorials', to: 'pages#tutorials', as: 'tutorials'
+  get '/about', to: 'pages#about', as: 'about'
 
 end
