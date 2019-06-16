e8d197458f14e07ef6ee22b9f04c0e7c3ad4a575
ivano panizza <ivano.panizza@libero.it>
2019-06-16 17:24:30 +0200

    plays controller updated

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,15 @@
 class PlaysController < ApplicationController
+  def index
+    @plays = Play.all
+  end
+
+  def show
+    @play = Play.find(params[:id])
+  end
+
+  def new
+    @play = Play.new
+  end
+
+
 end
